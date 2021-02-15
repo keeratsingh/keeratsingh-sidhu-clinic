81d2c4ee9b58ad9937b86531c52e3e97626859cc
Keerat Singh <keeratsingh@outlook.com>
2021-02-15 00:49:50 -0800

    Added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  root 'patients#index'
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'patients#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :patients do
     resources :prescriptions, :only => [:new, :edit, :create, :update, :destroy]
   end
