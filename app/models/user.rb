d5734c91e809c0ea33bdfc9c158fc8cdeb5d7471
Keerat Singh <keeratsingh@outlook.com>
2021-03-06 18:52:11 -0800

    Added login with username instead of password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :validatable
   devise :database_authenticatable, :rememberable, :validatable
+
+ def email_required?
+    false
+ end
+  
+ def email_changed?
+    false
+ end
 end
