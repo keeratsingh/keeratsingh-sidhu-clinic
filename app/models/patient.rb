657f1a822926e1276d659ed98b7cbbfabc178b21
Keerat Singh <keeratsingh@outlook.com>
2021-03-07 00:19:53 -0800

    Check patient uniqueness.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,12 @@
 class Patient < ApplicationRecord
     has_many :prescriptions, dependent: :destroy
-    validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Please enter the phone number in format (xxx-xxx-xxxx)" }
+    validates :phone_number,
+     format:
+     {
+       with: /\d{3}-\d{3}-\d{4}/,
+       message: "Please enter the phone number in format (xxx-xxx-xxxx)" 
+     }
+     validates_uniqueness_of :first_name,
+      scope: [:last_name, :dob, :phone_number],
+      message: 'A Patient with these details already exists.'
 end
